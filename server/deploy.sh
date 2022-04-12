7e41cb29f3249fcb1f341a999fc794224bf5c468
salem.affa <salem.affa@mehriso.com>
2022-04-12 03:40:09 +0100

    wip

---
+++ server/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+pm2 start dist/apps/sigrh-server/apps/sigrh-server/src/main.js
+pm2 start dist/apps/qrcode-generator/apps/qrcode-generator/src/main.js
+pm2 start dist/apps/score-manager/apps/score-manager/src/main.js
+pm2 start dist/apps/report/main.js
