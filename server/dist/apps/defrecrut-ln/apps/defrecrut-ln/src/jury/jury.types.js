c8f2f44b3538e4bd75d173d4cf868154d103f4a9
defense229 <defense229@gmail.com>
2022-04-22 11:05:06 +0100

    wip

--- server/dist/apps/defrecrut-ln/apps/defrecrut-ln/src/jury/jury.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=jury.types.js.map
--- server/dist/apps/sigrh-server/apps/sigrh-server/src/candidat/candidat_.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CandidatDto = void 0;
-class CandidatDto {
-}
-exports.CandidatDto = CandidatDto;
-//# sourceMappingURL=candidat_.dto.js.map
