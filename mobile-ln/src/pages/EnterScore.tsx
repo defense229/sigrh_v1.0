e0f8900e27c342507d192299ef0526505da839f8
salem.affa <salem.affa@mehriso.com>
2022-06-06 15:07:45 +0100

    bug fixed

--- mobile-ln/src/pages/EnterScore.tsx
+++ mobile-ln/src/pages/EnterScore.tsx
@@ -55,10 +55,20 @@ function EnterScore({
     } else {
       console.log(opt_questions);
       if (opt_questions.length === 0) {
+        setLoading(true);
+        await axios.post(config.api_url.defrecrutLn + 'questions/add-score', {
+          exam: questions[index - 1].exam,
+          field: questions[index - 1].id,
+          candidate: candidate.id,
+          value: Number(score),
+          extras: user.id,
+          isOptional: false,
+        });
+        setLoading(false);
         setOpenToast(true);
         setTimeout(() => {
           onFinish();
-        }, 2000);
+        }, 1000);
       } else {
         const oq = opt_questions[opt_index - 1];
 
@@ -106,53 +116,63 @@ function EnterScore({
 
   return (
     <div className="my-20 mx-10 bg-white radius-8 p-10">
-      <div className="fs-18 bold mb-8">
-        Attribution de notes (
-        {index <= questions.length
-          ? candidate.language
-          : candidate.optionalLanguage}
-        )
-      </div>
-      <hr />
-
-      <Condition cond={index <= questions.length}>
-        <div className="fs-18 bold mt-10 mb-4">
-          Question N° {index}/{questions.length}
+      <Condition cond={!openToast}>
+        <div className="fs-18 bold mb-8">
+          Attribution de notes (
+          {index <= questions.length
+            ? candidate.language
+            : candidate.optionalLanguage}
+          )
         </div>
-        <div className="fs-16" style={{ fontWeight: '500' }}>
-          {index <= questions.length ? questions[index - 1].label : ''}
+        <hr />
+
+        <Condition cond={index <= questions.length}>
+          <div className="fs-18 bold mt-10 mb-4">
+            Question N° {index}/{questions.length}
+          </div>
+          <div className="fs-16" style={{ fontWeight: '500' }}>
+            {index <= questions.length ? questions[index - 1].label : ''}
+          </div>
+        </Condition>
+
+        <Condition cond={index > questions.length}>
+          <div className="fs-18 bold mt-10 mb-4">
+            Question N° {opt_index}/{opt_questions.length}
+          </div>
+          <div className="fs-16" style={{ fontWeight: '500' }}>
+            {opt_index <= opt_questions.length
+              ? opt_questions[opt_index - 1].label
+              : ''}
+          </div>
+        </Condition>
+
+        <div className="my-10 mt-20">
+          <Input
+            label="Note"
+            type="number"
+            value={score}
+            placeholder="Saisir la note ici"
+            onChange={(e) => setScore(e.target.value)}
+          />
+        </div>
+
+        <div className="mt-30">
+          <Button
+            disabled={!(Number(score) >= 0 && Number(score) <= 20)}
+            expand
+            onClick={handleNext}>
+            Valider
+          </Button>
         </div>
       </Condition>
 
-      <Condition cond={index > questions.length}>
-        <div className="fs-18 bold mt-10 mb-4">
-          Question N° {opt_index}/{opt_questions.length}
-        </div>
-        <div className="fs-16" style={{ fontWeight: '500' }}>
-          {opt_index <= opt_questions.length
-            ? opt_questions[opt_index - 1].label
-            : ''}
-        </div>
+      <Condition cond={openToast}>
+        <ComponentLoading />
       </Condition>
 
-      <div className="my-10 mt-20">
-        <Input
-          label="Note"
-          type="number"
-          value={score}
-          placeholder="Saisir la note ici"
-          onChange={(e) => setScore(e.target.value)}
-        />
-      </div>
-
-      <div className="mt-30">
-        <Button
-          disabled={!(Number(score) >= 0 && Number(score) <= 20)}
-          expand
-          onClick={handleNext}>
-          Valider
-        </Button>
-      </div>
+      <Toast open={openToast} ypos="bottom" xpos="left">
+        Toutes les questions du candidats viennent d'être prise en compte.
+      </Toast>
     </div>
   );
 }
